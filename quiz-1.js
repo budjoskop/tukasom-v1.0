895393d0daecc7066f3f04589253c98595e9ead0
budjoskop <31095126+budjoskop@users.noreply.github.com>
2017-12-12 22:01:35 +0100

    Add files via upload

--- quiz-1.js
+++ quiz-1.js
@@ -15,13 +15,15 @@ else {
             
 // ovde se menja kviz
 
-var cat1 = ($("input[@name=q1]:checked").val() != "b"); 
-var cat2 = ($("input[@name=q1]:checked").val() == "b");           
+var cat1 = ($("input[@name=q1]:checked").val() != "a"); 
+var cat2 = ($("input[@name=q1]:checked").val() == "a");         
+
 
 
                      
 if (cat1) { $("#category1").show("slow");$("#category2").hide("slow"); };   
-if (cat2) { $("#category2").show("slow"); $("#category1").hide("slow"); };                         
+if (cat2) { $("#category2").show("slow"); $("#category1").hide("slow"); };    
+                         
 
 { $("#closing").show("slow"); };
 }
