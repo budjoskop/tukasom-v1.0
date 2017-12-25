1ae0bdecd87e004caa7222ebc02acb8f6d2c6279
budjoskop <gtomicognjen@gmail.com>
2017-12-25 09:58:06 +0100

    pornici

--- quiz-1.js
+++ quiz-1.js
@@ -1,29 +1,29 @@
-$(document).ready(function() 
-{    $("#check").click(function() {                
+$(document).ready(function()
+{    $("#check").click(function() {
 
-if (!$("input[@name=q1]:checked").val()            
-           
-) {            
-alert("You're not done yet!");        
-}        
+if (!$("input[@name=q1]:checked").val()
 
-else {            
-  
+) {
+alert("You're not done yet!");
+}
+
+else {
+
   function eraseText() {
     document.getElementById("check").value = "";
-}                        
-            
+}
+
 // ovde se menja kviz
 
-var cat1 = ($("input[@name=q1]:checked").val() != "a"); 
-var cat2 = ($("input[@name=q1]:checked").val() == "a");         
+var cat1 = ($("input[@name=q1]:checked").val() != "b");
+var cat2 = ($("input[@name=q1]:checked").val() == "b");         
 
 
 
-                     
-if (cat1) { $("#category1").show("slow");$("#category2").hide("slow"); };   
-if (cat2) { $("#category2").show("slow"); $("#category1").hide("slow"); };    
-                         
+
+if (cat1) { $("#category1").show("slow");$("#category2").hide("slow"); };
+if (cat2) { $("#category2").show("slow"); $("#category1").hide("slow"); };
+
 
 { $("#closing").show("slow"); };
 }
