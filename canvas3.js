d19e1871e9127a8866bde8c1875c34b4d6609418
budjoskop <gtomicognjen@gmail.com>
2018-01-04 10:50:50 +0100

    kosmos

--- canvas3.js
+++ canvas3.js
@@ -1,112 +1,184 @@
-window.requestAnimFrame = (function(){
-    return  window.requestAnimationFrame       ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-            window.oRequestAnimationFrame      ||
-            window.msRequestAnimationFrame     ||
-            function (callback) { window.setTimeout(callback, 1000 / 60); };
+/*
+(Book REF) HTML5 Canvas Ch. 5:  Math, Physics, and Animation ::: Uniform Circular Motion
+By Steve Fulton and Jeff Fulton
+*/
+window.requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
 })();
+window.addEventListener('load', start, false);
 
-var c = document.getElementById("canvas");
-var $=c.getContext("2d");
+var c,
+  $,
+  w,
+  h,
+  msX,
+  msY,
+  midX,
+  midY,
+  num = 650,
+  parts = [],
+  begin = 50,
+  repeat = 20,
+  end = Math.PI * 2,
+  force = null,
+  msdn = false;
 
-var W=c.width=window.innerWidth;
-var H=c.height = window.innerHeight;
+function start() {
+  c = document.getElementById('canvas');
+  $ = c.getContext('2d');
+  w = c.width = window.innerWidth;
+  h = c.height = window.innerHeight;
+  midX = w / 2;
+  midY = h / 2;
+  force = Math.max(w, h) * 0.09;
+  flow = begin;
 
-var wheels = [];
-var grads = [];
-
-var go = function(){
-  for(var i = 0; i < 50; i++){
-    wheels.push(new Wheel(Math.random()*W, Math.random()*H, Math.random()*10-5,Math.random()*10-5, Math.random()*20+20, Math.random()*255>>0,Math.random()*255>>0,Math.random()*255>>0));
-  grads.push("rgba(" + 255 + "," + 255 + "," + 255 + ",");
-  grads.push("rgba(" + 0 + "," + 0 + "," + 0 + ",");
-  grads.push("rgba(" + wheels[i].r + "," + wheels[i].g + "," + wheels[i].b + ",");
-  }
+  window.requestAnimFrame(create);
   run();
 }
-var run = function(){
+
+function run() {
   window.requestAnimFrame(run);
-  draw();
+  go();
 }
-var draw = function(){
-  $.globalCompositeOperation = "source-over";
-  $.fillStyle = "rgba(0, 0, 0, 0.5)";
-  $.fillRect(0, 0, W, H);
 
-  $.globalCompositeOperation = "lighter";
+function Part() {
+  this.deg = 0;
+  this.rad = 0;
+  this.x = 0;
+  this.y = 0;
+  this.distX = 0;
+  this.distY = 0;
+  this.color = 'rgb(' + Math.floor(Math.random() * 130) + ',' + Math.floor(Math.random() * 50) + ',' + Math.floor(Math.random() * 100) + ')';
+  this.size;
+}
 
-  for(i=0; i<wheels.length; i++){
+function create() {
+  var n = num;
+  while (n--) {
+    var p = new Part();
+    p.deg = Math.floor(Math.random() * 360);
+    p.rad = Math.floor(Math.random() * w * 0.5);
+    p.x = p.distX = Math.floor(Math.random() * w);
+    p.y = p.distY = Math.floor(Math.random() * h);
+    p.size = 1 + Math.floor(Math.random() * (p.rad * 0.055));
+    parts[n] = p;
+  }
+  c.onmousemove = msmv;
+  c.onmousedown = msdn;
+  c.onmouseup = msup;
+
+  var int = setInterval(function() {
+    flow--;
+    if (flow === repeat) clearInterval(int);
+  }, 20);
+}
+
+function go() {
+  $.globalCompositeOperation = 'source-over';
+  $.fillStyle = 'hsla(242, 30%, 5%, .55)';
+  $.fillRect(0, 0, w, h);
+  $.globalCompositeOperation = 'lighter';
+  var mx = msX;
+  var my = msY;
+  var bounds = force;
+  if (msdn) {
+    bounds = force * 2;
+  }
+  var n = num;
+  while (n--) {
+    var p = parts[n];
+    var radi = Math.PI / 180 * p.deg;
+    p.distX = midX + p.rad * Math.cos(radi);
+    p.distY = midY + p.rad * Math.sin(radi) * 0.4;
+    if (mx && my) {
+      var react = Math.floor((bounds * 0.5) + Math.random() * (bounds * 0.9));
+      if (p.distX - mx > 0 &&
+        p.distX - mx < bounds &&
+        p.distY - my > 0 &&
+        p.distY - my < bounds) {
+        p.distX += react;
+        p.distY += react;
+      } else if (p.distX - mx > 0 &&
+        p.distX - mx < bounds &&
+        p.distY - my < 0 &&
+        p.distY - my > -bounds) {
+        p.distX += react;
+        p.distY -= react;
+      } else if (p.distX - mx < 0 &&
+        p.distX - mx > -bounds &&
+        p.distY - my > 0 &&
+        p.distY - my < bounds) {
+        p.distX -= react;
+        p.distY += react;
+      } else if (p.distX - mx < 0 &&
+        p.distX - mx > -bounds &&
+        p.distY - my < 0 &&
+        p.distY - my > -bounds) {
+        p.distY -= react;
+        p.distY -= react;
+      }
+    }
+    p.x += ((p.distX - p.x) / flow);
+    p.y += ((p.distY - p.y) / flow);
+    var x = p.x;
+    var y = p.y;
+    var s = p.size * (p.y * 1.5 / h);
+    if (s < 0.1) {
+      s = 0;
+    }
     $.beginPath();
-  var w = wheels[i];
-    var gradblur = $.createRadialGradient(w.x, w.y, 0, w.x, w.y, w.size);
-        w.alpha += (w.toAlpha-w.alpha)/4.0;
-        if(w.alpha > 1) w.alpha = 1;
-
-    gradblur.addColorStop(0,grads[i*3+1]+(w.alpha)+")");
-    gradblur.addColorStop(0.4,grads[i*3+1]+(w.alpha)+")");
-    gradblur.addColorStop(0.65,grads[i*3+2]+(w.alpha*0.9)+")");
-    gradblur.addColorStop(0.65,grads[i*3]+(w.alpha)+")");
-    gradblur.addColorStop(0.75,grads[i*3]+(w.alpha)+")");
-    gradblur.addColorStop(0.75,grads[i*3+2]+(w.alpha*0.9)+")");
-    gradblur.addColorStop(1,grads[i*3+1]+(w.alpha)+")");
-    w.toAlpha = 0;
-    $.fillStyle = gradblur;
-    $.arc(w.x, w.y, w.size, 0, Math.PI*2, false);
+    $.fillStyle = p.color;
+    $.arc(x, y, s, 0, end, true);
     $.fill();
-    w.x += w.vx;
-    w.y += w.vy;
-
-    if(w.x < -20) w.x = W+20;
-    if(w.y < -20) w.y = H+20;
-    if(w.x > W+20) w.x = -20;
-    if(w.y > H+20) w.y = -20;
-
-  for (var j = i + 1; j < 50; j++) {
-    spokes(w, wheels[j]);
-  }
-  }
-}
-
-var spokes = function(w1, w2){
-  var dx = w2.x - w1.x;
-  var dy = w2.y - w1.y;
-  var dist = Math.sqrt(dx * dx + dy * dy);
-  if(dist < 100){
-    var grad = $.createLinearGradient(w1.x, w1.y, w2.x, w2.y);
-    grad.addColorStop(0,'rgba('+w1.r+','+w1.g+','+w1.b+','+(1-dist/100)+')');
-    grad.addColorStop(1,'rgba('+w2.r+','+w2.g+','+w2.b+','+(1-dist/100)+')');
-    $.strokeStyle = grad;
-    $.beginPath();
-    $.moveTo(w1.x,w1.y);
-    $.lineTo(w2.x,w2.y);
     $.closePath();
-    $.stroke();
-    w1.toAlpha += 0.1;
-    w2.toAlpha += 0.1;
-    var ax = dx * 0.0075;
-    var ay = dy * 0.0075;
-    w1.vx += ax / w1.size;
-    w1.vy += ay / w1.size;
-    w2.vx -= ax / w2.size;
-    w2.vy -= ay / w2.size;
+    var vary;
+    if (p.size < 2) {
+      vary = 4;
+    } else if (p.size < 3) {
+      vary = 3;
+    } else if (p.size < 4) {
+      vary = 2;
+    } else {
+      vary = 1;
+    }
+    vary *= (p.y / (h * 0.9));
+    p.deg += vary;
+    p.deg = p.deg % 360;
   }
 }
 
-var Wheel = function(x,y,vx,vy,size,r,g,b){
-  this.r = r;
-  this.g = g;
-  this.b = b;
-  this.x = x;
-  this.y = y;
-  this.vx = vx;
-  this.vy = vy;
-  this.size = size;
-  this.toAlpha=0.0;
-  this.alpha = 1.0;
+function msmv(e) {
+  var p = getPos(e.target);
+  var sX = window.pageXOffset;
+  var sY = window.pageYOffset;
+  msX = e.clientX - p.x + sX;
+  msY = e.clientY - p.y + sY;
 }
-window.addEventListener('resize', function(){
-  c.width = W = window.innerWidth;
-  c.height = H = window.innerHeight;
-});
-go();
+
+function msdn(e) {
+  msdn = true;
+}
+
+function msup(e) {
+  msdn = false;
+}
+
+function getPos(el) {
+  var cosmo = {};
+  cosmo.x = el.offsetLeft;
+  cosmo.y = el.offsetTop;
+  while (el.offsetParent) {
+    el = el.offsetParent;
+    cosmo.x += el.offsetLeft;
+    cosmo.y += el.offsetTop;
+  }
+  return cosmo;
+}
