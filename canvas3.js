bbdfe2752c425e021439188e22f0b8718224273e
budjoskop <gtomicognjen@gmail.com>
2018-01-04 10:59:12 +0100

    hipno

--- canvas3.js
+++ canvas3.js
@@ -1,184 +1,95 @@
-/*
-(Book REF) HTML5 Canvas Ch. 5:  Math, Physics, and Animation ::: Uniform Circular Motion
-By Steve Fulton and Jeff Fulton
-*/
 window.requestAnimFrame = (function() {
   return window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
     window.mozRequestAnimationFrame ||
     window.oRequestAnimationFrame ||
     window.msRequestAnimationFrame ||
-    function(callback) {
+    function(loop) {
       window.setTimeout(callback, 1000 / 60);
     };
 })();
-window.addEventListener('load', start, false);
 
-var c,
-  $,
-  w,
-  h,
-  msX,
-  msY,
-  midX,
-  midY,
-  num = 650,
-  parts = [],
-  begin = 50,
-  repeat = 20,
-  end = Math.PI * 2,
-  force = null,
-  msdn = false;
+var fps = 30;
 
-function start() {
-  c = document.getElementById('canvas');
-  $ = c.getContext('2d');
-  w = c.width = window.innerWidth;
-  h = c.height = window.innerHeight;
-  midX = w / 2;
-  midY = h / 2;
-  force = Math.max(w, h) * 0.09;
-  flow = begin;
+var Circle = function(_dir) {
+  _dir = _dir || {};
 
-  window.requestAnimFrame(create);
-  run();
-}
+  this._begin = _dir._begin;
+  this._fin = this._begin;
+  this._add = Math.PI * 4 / fps / 3;
 
-function run() {
-  window.requestAnimFrame(run);
-  go();
-}
+  this.r = _dir.r;
 
-function Part() {
-  this.deg = 0;
-  this.rad = 0;
-  this.x = 0;
-  this.y = 0;
-  this.distX = 0;
-  this.distY = 0;
-  this.color = 'rgb(' + Math.floor(Math.random() * 130) + ',' + Math.floor(Math.random() * 50) + ',' + Math.floor(Math.random() * 100) + ')';
-  this.size;
-}
+  this.go();
+};
 
-function create() {
-  var n = num;
-  while (n--) {
-    var p = new Part();
-    p.deg = Math.floor(Math.random() * 360);
-    p.rad = Math.floor(Math.random() * w * 0.5);
-    p.x = p.distX = Math.floor(Math.random() * w);
-    p.y = p.distY = Math.floor(Math.random() * h);
-    p.size = 1 + Math.floor(Math.random() * (p.rad * 0.055));
-    parts[n] = p;
+Circle.prototype.go = function() {};
+
+var Set = function() {
+  this._c_ = [];
+  this.go();
+};
+
+Set.prototype.go = function() {
+  this.c = document.getElementById("canvas");
+
+  this.$ = this.c.getContext("2d");
+
+  this.c.width = window.innerWidth;
+  this.c.height = window.innerHeight;
+
+  for (var i = 1; i < 12; i++) {
+
+    var _dir = {
+      _begin: Math.PI * 2 / 12 * i,
+      r: 25 * i
+    };
+    var _c = new Circle(_dir);
+    this._c_.push(_c);
+
   }
-  c.onmousemove = msmv;
-  c.onmousedown = msdn;
-  c.onmouseup = msup;
 
-  var int = setInterval(function() {
-    flow--;
-    if (flow === repeat) clearInterval(int);
-  }, 20);
-}
+  this.$.fillStyle = "hsla(0,0%,0%,1)";
+  this.$.fillRect(0, 0, this.c.width, this.c.height);
+  this.$.globalCompositeOperation = 'source-over';
 
-function go() {
-  $.globalCompositeOperation = 'source-over';
-  $.fillStyle = 'hsla(242, 30%, 5%, .55)';
-  $.fillRect(0, 0, w, h);
-  $.globalCompositeOperation = 'lighter';
-  var mx = msX;
-  var my = msY;
-  var bounds = force;
-  if (msdn) {
-    bounds = force * 2;
-  }
-  var n = num;
-  while (n--) {
-    var p = parts[n];
-    var radi = Math.PI / 180 * p.deg;
-    p.distX = midX + p.rad * Math.cos(radi);
-    p.distY = midY + p.rad * Math.sin(radi) * 0.4;
-    if (mx && my) {
-      var react = Math.floor((bounds * 0.5) + Math.random() * (bounds * 0.9));
-      if (p.distX - mx > 0 &&
-        p.distX - mx < bounds &&
-        p.distY - my > 0 &&
-        p.distY - my < bounds) {
-        p.distX += react;
-        p.distY += react;
-      } else if (p.distX - mx > 0 &&
-        p.distX - mx < bounds &&
-        p.distY - my < 0 &&
-        p.distY - my > -bounds) {
-        p.distX += react;
-        p.distY -= react;
-      } else if (p.distX - mx < 0 &&
-        p.distX - mx > -bounds &&
-        p.distY - my > 0 &&
-        p.distY - my < bounds) {
-        p.distX -= react;
-        p.distY += react;
-      } else if (p.distX - mx < 0 &&
-        p.distX - mx > -bounds &&
-        p.distY - my < 0 &&
-        p.distY - my > -bounds) {
-        p.distY -= react;
-        p.distY -= react;
-      }
-    }
-    p.x += ((p.distX - p.x) / flow);
-    p.y += ((p.distY - p.y) / flow);
-    var x = p.x;
-    var y = p.y;
-    var s = p.size * (p.y * 1.5 / h);
-    if (s < 0.1) {
-      s = 0;
-    }
-    $.beginPath();
-    $.fillStyle = p.color;
-    $.arc(x, y, s, 0, end, true);
-    $.fill();
-    $.closePath();
-    var vary;
-    if (p.size < 2) {
-      vary = 4;
-    } else if (p.size < 3) {
-      vary = 3;
-    } else if (p.size < 4) {
-      vary = 2;
+};
+
+Set.prototype.draw = function() {
+  this.$.fillStyle = "hsla(0,0%,5%,.1)";
+  this.$.fillRect(0, 0, this.c.width, this.c.height);
+  this.$.lineWidth = 18;
+  this.$.lineCap = 'round';
+
+  for (var i in this._c_) {
+
+    this.$.beginPath();
+    this.$.strokeStyle = 'hsla(' + i + 12 + ',100%, 50%,1)';
+    if (this._c_[i]._begin > Math.PI * 2) {
+      this._c_[i]._begin = 0;
+      this._c_[i]._fin = 0;
     } else {
-      vary = 1;
+      this._c_[i]._begin = this._c_[i]._fin;
+      this._c_[i]._fin += this._c_[i]._add;
     }
-    vary *= (p.y / (h * 0.9));
-    p.deg += vary;
-    p.deg = p.deg % 360;
+    this.$.arc(this.c.width / 2, this.c.height / 2, this._c_[i].r, this._c_[i]._begin, this._c_[i]._fin, false);
+    this.$.stroke();
+    this.$.closePath();
   }
-}
 
-function msmv(e) {
-  var p = getPos(e.target);
-  var sX = window.pageXOffset;
-  var sY = window.pageYOffset;
-  msX = e.clientX - p.x + sX;
-  msY = e.clientY - p.y + sY;
-}
+};
 
-function msdn(e) {
-  msdn = true;
-}
+Set.prototype.ready = function() {
+  var hc = this;
+  setInterval(function() {
+    hc.draw();
+  }, fps);
+};
 
-function msup(e) {
-  msdn = false;
-}
+var ready = new Set();
+ready.ready();
 
-function getPos(el) {
-  var cosmo = {};
-  cosmo.x = el.offsetLeft;
-  cosmo.y = el.offsetTop;
-  while (el.offsetParent) {
-    el = el.offsetParent;
-    cosmo.x += el.offsetLeft;
-    cosmo.y += el.offsetTop;
-  }
-  return cosmo;
-}
+window.addEventListener('resize', function() {
+  c.width = w = window.innerWidth;
+  c.height = h = window.innerHeight;
+});
