6f0fd3106236f469a99b610debc09383f2d6ca22
budjoskop <gtomicognjen@gmail.com>
2018-01-25 11:41:38 +0100

    gorila

--- canvas3.js
+++ canvas3.js
@@ -1,43 +1,47 @@
-var c = document.getElementById('canvas');
-var ctx = c.getContext('2d');
-var w = c.width = window.innerWidth;
-var h = c.height = window.innerHeight;
-F = 0;
-run();
+var canvasLoader = function(){
 
-function D() {
-  F++;
-  ctx.fillStyle = 'hsla(0,0%,95%,0.8)';
-  ctx.fillRect(0, 0, w, h);
-  for (i = 50; i--;) {
-    t = F / -8 - i * 6;
-    ctx.fillStyle = 'hsla(0, 0%, 5%,1)';
-    E(-50 + i * Math.cos(t), 8 * i + 3 * Math.sin(t));
-    E(125 + i * Math.cos(t), 4 * i + 3 * Math.sin(t));
+  var self = this;
+  window.requestAnimFrame=function(){return window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame||window.oRequestAnimationFrame||window.msRequestAnimationFrame||function(a){window.setTimeout(a,1E3/60)}}();
 
-  }
+  self.init = function(){
+    self.canvas = document.getElementById('canvas');
+    self.ctx = self.canvas.getContext('2d');
+    self.ctx.lineWidth = .5;
+    self.ctx.strokeStyle = 'rgba(0,0,0,.75)';
+    self.count = 75;
+    self.rotation = 270*(Math.PI/180);
+    self.speed = 6;
+    self.canvasLoop();
+  };
+
+  self.updateLoader = function(){
+    self.rotation += self.speed/100;
+  };
+
+  self.renderLoader = function(){
+    self.ctx.save();
+    self.ctx.globalCompositeOperation = 'source-over';
+    self.ctx.translate(125, 125);
+    self.ctx.rotate(self.rotation);
+    var i = self.count;
+    while(i--){
+      self.ctx.beginPath();
+      self.ctx.arc(0, 0, i+(Math.random()*35), Math.random(), Math.PI/3+(Math.random()/12), false);
+      self.ctx.stroke();
+    }
+    self.ctx.restore();
+  };
+
+  self.canvasLoop = function(){
+    requestAnimFrame(self.canvasLoop, self.canvas);
+    self.ctx.globalCompositeOperation = 'destination-out';
+    self.ctx.fillStyle = 'rgba(0,0,0,.03)';
+    self.ctx.fillRect(0,0,250,250);
+    self.updateLoader();
+    self.renderLoader();
+  };
+
 };
-function E(a, b) {
-  G(a, b);
-  G(-a, b);
-  G(-a, -b);
-  G(a, -b);
-  G(b, -a);
-  G(-b, -a);
-  G(-b, a);
-  G(b, a);
-}
 
-function G(a, b) {
-  ctx.beginPath();
-  ctx.arc(w / 2 + a, h / 2 + b, 1, 0, Math.PI*2, false);
-  ctx.fill();
-}
-window.addEventListener('resize',function(){
-  c.width = w = window.innerWidth;
-  c.height = h =  window.innerHeight;
-}, false);
-function run() {
-  window.requestAnimationFrame(run);
-  D();
-}
+var loader = new canvasLoader();
+loader.init();
